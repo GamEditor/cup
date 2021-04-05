c09573ac2d1230865cb41422b74f472b273aa4cc
Mohammad <scientist.us@gmail.com>
2021-04-05 23:45:38 +0430

    initial commit

---
+++ Cup/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Cup
+{
+    class Program
+    {
+        [DllImport("shell32.dll")]
+        private static extern long FindExecutable(string lpFile, string lpDirectory, StringBuilder lpResult);
+
+        private static void Main(string[] args)
+        {
+            RegisteryWriter.RegisterThisApp();
+
+            if (args.Length > 0)
+            {
+                string url = "";
+
+                if (args[0].IndexOf("cup://") == 0)
+                {
+                    url = args[0].Replace("cup://", "");
+                }
+                else if (args[0].IndexOf("cup:") == 0)
+                {
+                    url = args[0].Replace("cup:", "");
+                }
+                else
+                {
+                    return;
+                }
+
+                Process.Start("chrome.exe", url);
+            }
+        }
+    }
+}
---
+++ Cup/RegisteryWriter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+
+namespace Cup
+{
+    public static class RegisteryWriter
+    {
+        public static void RegisterThisApp()
+        {
+            string fileLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(@"cup"))
+            {
+                key.SetValue("", "URL:cup Protocol");   // (Default)
+                key.SetValue("URL Protocol", "cup");
+                key.Close();
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(@"cup\DefaultIcon"))
+            {
+                key.SetValue("", fileLocation);
+                key.Close();
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(@"cup\shell"))
+            {
+                key.Close();
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(@"cup\shell\open"))
+            {
+                key.Close();
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(@"cup\shell\open\command"))
+            {
+                key.SetValue("", fileLocation + " \"%1\"");
+                key.Close();
+            }
+        }
+    }
+}
